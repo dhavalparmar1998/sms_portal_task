6a402b36980ee914ee546adeed196f349b1addad
Dhavalparmar <dhavalparmar7727@gmail.com>
2024-12-02 17:29:29 +0530

    ejs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config'
-import ejs from 'ejs';
+// import ejs from 'ejs';
 // import homePage from './controllers/home/controller.js';
 import contactRoute from './routes/contact/routes.js';
 import msgRoute from './routes/message/routes.js';
@@ -9,6 +9,7 @@ import libraryRoute from './routes/library/routes.js';
 import dbconnect from './database/db.js';
 import groupRoute from './routes/group/routes.js';
 import homeRoute from './routes/home/routes.js';
+
 dbconnect()
   .then(() => {
     console.log('Connected to MongoDB');
@@ -19,7 +20,7 @@ dbconnect()
 
 const app = express();
 
-app.set("view engine", ejs)
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded());
 app.use("/public", express.static("public"))
 
